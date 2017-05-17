46168aefbc94ad36a1b7377aa2ad4e5bac6eb4ae
Marta Delgado <martadelgado@Martas-MacBook-Air.local>
2017-05-17 16:23:45 +0200

    not done

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,3 +1,32 @@
+window.onload = function() {
+
+  var deleteProd = document.getElementById("delete");
+
+  deleteProd.onclick = function deleteProduct() {
+    console.log("hey");
+  };
+
+  var qty = document.getElementsByClassName("input");
+  var unitPrice = document.getElementsByClassName("cost");
+  var calculate = document.getElementById("calculatePriceButton");
+  var priceResult = [];
+
+  calculate.onclick = function getPriceByProduct(){
+      for(var i = 0; i < unitPrice.length; i++){
+        var totalPrice = (qty[i].value)*(unitPrice[i].textContent);
+        priceResult.push(totalPrice);
+        document.getElementsByClassName("total")[i].innerHTML = ("$" + priceResult[i]);
+      }
+  function getTotal() {
+    for(var i = 0; i < priceResult.length; i++) {
+      finalPrice += priceResult;
+        console.log("total", pricerResult);
+    }
+  }
+  };
+};
+
+
 // window.onload = function(){
 //   var calculatePriceButton = document.getElementById('calc-prices-button');
 //   var createItemButton = document.getElementById('new-item-create');
@@ -20,17 +49,17 @@
 
 
 
-var qtyInput = document.getElementsByClassName("unitInput");
-var unitPrice = document.getElementsByClassName("productPrice");
-var priceResult = [];
-
-function getPriceByProduct(){
-  for(var i = 0; i < unitPrice.length; i++){
-    var totalPrice = (qtyInput[i].value)*(unitPrice[i].textContent);
-    priceResult.push(totalPrice);
-    document.getElementsByClassName("totalPriceProd")[i].innerHTML = ("$" + priceResult[i]);
-  }
-}
+// var qtyInput = document.getElementsByClassName("input");
+// var unitPrice = document.getElementsByClassName("productPrice");
+// var priceResult = [];
+//
+// function getPriceByProduct(){
+//   for(var i = 0; i < unitPrice.length; i++){
+//     var totalPrice = (qtyInput[i].value)*(unitPrice[i].textContent);
+//     priceResult.push(totalPrice);
+//     document.getElementsByClassName("totalPriceProd")[i].innerHTML = ("$" + priceResult[i]);
+//   }
+// }
 
 
 //
@@ -50,26 +79,26 @@ function getPriceByProduct(){
 //     return getPrice(itemField) * getQuantity(quantityField);
 // }
 
-function createQuantityInput(){
-
-}
-
-function createDeleteButton(){
-
-}
-
-function createQuantityNode(){
-
-}
-
-function createItemNode(dataType, itemData){
-
-}
-
-function createNewItemRow(itemName, itemUnitPrice){
-
-}
-
-function createNewItem(){
-
-}
+// function createQuantityInput(){
+//
+// }
+//
+// function createDeleteButton(){
+//
+// }
+//
+// function createQuantityNode(){
+//
+// }
+//
+// function createItemNode(dataType, itemData){
+//
+// }
+//
+// function createNewItemRow(itemName, itemUnitPrice){
+//
+// }
+//
+// function createNewItem(){
+//
+// }
